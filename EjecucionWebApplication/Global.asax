1e4c35d4aa7cf9687a61be1a202713a9a71f6249
joseamarfilaguirre <joseamarfilaguirre@outlook.com>
2019-01-17 08:50:46 -0300

    Agregar archivos de proyecto.

---
+++ EjecucionWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EjecucionWebApplication.MvcApplication" Language="C#" %>
---
+++ EjecucionWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EjecucionWebApplication.Startup))]
+namespace EjecucionWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
